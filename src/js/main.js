9a3c1db573529eb33247531ca38b6a9beba0a7f8
Alvaro <astaboada7@gmail.com>
2021-01-04 12:33:35 +0100

    canvas and liitle ball there

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,40 @@
-import alerta from 'alert';
 
-alerta();
+let littleBall;
+
+function startGame() {
+    littleBall = new component (15, 15, "blue", 220, 160);
+    myGameArea.start();
+}
+
+let myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 1100;
+        this.canvas.height = 200;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[4]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    littleBall.x += 1;
+    littleBall.update();
+}
